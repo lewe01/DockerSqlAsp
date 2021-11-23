162bab6b44857a0cdba24a4311140902dfd3fe65
lewe01 <lewel4040@gmail.com>
2021-11-23 14:19:35 +0300

    file uploaded

---
+++ DockerSqlAsp/Models/FilmContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DockerSqlAsp.Models {
+    public class FilmContext : DbContext {
+        public FilmContext(DbContextOptions<FilmContext> opt_Db) : base(opt_Db) {
+        }
+        public DbSet<Film> Film { get; set; }
+    }
+}
